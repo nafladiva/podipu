a1f7d962bb59c506f59fef9a233d4fa23be8360c
Nafla Diva Syafia <nafladiva@gmail.com>
2023-12-03 12:44:45 +0700

    refactor: remove unused assets

--- lib/shared/data/dummy_data.dart
+++ lib/shared/data/dummy_data.dart
@@ -1,47 +1,7 @@
 import 'package:podipu/shared/data/models/episode_mdl.dart';
 import 'package:podipu/shared/data/models/podcast_mdl.dart';
-import 'package:podipu/shared/consts/asset_path.dart';
-import 'package:podipu/shared/data/models/podcast.dart';
 
 class DummyData {
-  static const List<Podcast> datas = [
-    Podcast(
-      id: 1,
-      title: 'Starboy',
-      artist: 'The Weeknd',
-      coverPath: AssetPath.starboyCoverImg,
-      audioPath: AssetPath.starboyAudio,
-    ),
-    Podcast(
-      id: 2,
-      title: 'Snooze',
-      artist: 'SZA',
-      coverPath: AssetPath.snoozeCoverImg,
-      audioPath: AssetPath.snoozeAudio,
-    ),
-    Podcast(
-      id: 3,
-      title: 'Tally',
-      artist: 'Blackpink',
-      coverPath: AssetPath.tallyCoverImg,
-      audioPath: AssetPath.tallyAudio,
-    ),
-    Podcast(
-      id: 4,
-      title: 'The Hills',
-      artist: 'The Weeknd',
-      coverPath: AssetPath.theHillsCoverImg,
-      audioPath: AssetPath.theHillsAudio,
-    ),
-    Podcast(
-      id: 5,
-      title: 'Try Me',
-      artist: 'The Weeknd',
-      coverPath: AssetPath.tryMeCoverImg,
-      audioPath: AssetPath.tryMeAudio,
-    ),
-  ];
-
   static const podcast = PodcastMdl(
     id: '4d3fe717742d4963a85562e9f84d8c79',
     imageUrl:
